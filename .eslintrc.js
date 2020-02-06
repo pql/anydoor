6b22ae4b19d7d1818860a45e3b62b00cc0baf6d1
pql <1101192991@qq.com>
2020-02-06 13:14:14 +0800

    first

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "extends": ["eslint:recommended"],
+  "parser": "babel-eslint",
+  "rules": {
+    "no-console": ["error", {
+      "allow": ["warn", "error", "info"]
+    }]
+  },
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "sourceType": "script"
+  },
+  "globals": {
+  },
+  "env": {
+    "node": true,
+    "es6": true,
+    "mocha": true
+  }
+}
